50b389a6690f8bac46b5fadc1da0aec5f13452ae
Bloom <bloomtauta@gmail.com>
2023-09-07 14:17:39 +0300

    done

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,11 @@
-def write_file(file_name, file_content):
-    pass
+def write_file(file_name="test_file", file_content="This is a test content."):
+    with open(f"{file_name}.txt", "w", encoding="utf-8") as write_file:
+        return write_file.write(file_content)
 
-def append_file(file_name, append_content):
-    pass
+def append_file(file_name="test_file", append_content="Appended content"):
+    appended_file = open(f"{file_name}.txt", "a", encoding="utf-8")
+    return appended_file.write(append_content)
 
-def read_file(file_name):
-    pass
+def read_file(file_name="test_file"):
+    read_file = open(f"{file_name}.txt", encoding="utf-8")
+    return read_file.read()
